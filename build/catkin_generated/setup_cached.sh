6c79d411d823e50f4235191ade6768080fb7468d
maolj <maoljchn@foxmail.com>
2023-07-12 15:49:53 +0800

    Upload the code for the first time

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/lory/fast_lio_sam_ws/src/FAST_LIO_SAM/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/lory/fast_lio_sam_ws/src/FAST_LIO_SAM/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/melodic/bin:/home/lory/.local/bin:/home/lory/matlab2020/bin:/home/lory/anaconda3/bin:/home/lory/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/cuda/bin'
+export PKG_CONFIG_PATH="/home/lory/fast_lio_sam_ws/src/FAST_LIO_SAM/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/lory/fast_lio_sam_ws/src/FAST_LIO_SAM/build'
+export PYTHONPATH="/home/lory/fast_lio_sam_ws/src/FAST_LIO_SAM/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/lory/fast_lio_sam_ws/src/FAST_LIO_SAM/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/lory/fast_lio_ws/注释版本code/FAST_LIO_NOTED-master:/home/lory/fast_lio_sam_ws/src/FAST_LIO_SAM:$ROS_PACKAGE_PATH"
